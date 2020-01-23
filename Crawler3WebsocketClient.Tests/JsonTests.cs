c2699fc5d383e51ba1d57eb0ea7eb8ada42f45b7
FF <@>
2020-01-23 14:11:22 +0100

    switch to new jsonserializer

---
+++ Crawler3WebsocketClient.Tests/JsonTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using NUnit.Framework;
+
+namespace Crawler3WebsocketClient.Tests {
+    public class JsonTests {
+
+        private class Json1Data {
+            public int X { get; set; }
+            public int Y { get; set; }  
+        }
+
+        [Test]
+        public void Json1() {
+            var o = new JsonSerializerOptions {
+                PropertyNameCaseInsensitive = true,
+            };
+            var d = JsonSerializer.Deserialize<Json1Data>("{\"x\":1, \"y\":2}", o);
+            TestContext.WriteLine(d.X);
+            Assert.AreEqual(1, d.X);
+        }
+
+    }
+}
--- Crawler3WebsocketClient/JsonProcessor.cs
+++ Crawler3WebsocketClient/JsonProcessor.cs
@@ -1,21 +1,24 @@
 ﻿using System;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Serialization;
+using System.Text.Json;
+
 
 namespace Crawler3WebsocketClient {
     public class JsonProcessor {
         private readonly IWebsocketLogger _logger;
-        private readonly JsonSerializer _serializer;
+        private readonly JsonSerializerOptions _serializerOptions;
         public JsonProcessor(IWebsocketLogger logger = null) {
             _logger = logger;
-            _serializer = JsonSerializer.CreateDefault();
-            _serializer.ContractResolver = new CamelCasePropertyNamesContractResolver();
-            _serializer.NullValueHandling = NullValueHandling.Ignore;   
+            _serializerOptions = new JsonSerializerOptions() {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = false,
+            };
         }
 
         public string Serialize(object o) {
-            return JToken.FromObject(o, _serializer).ToString(Formatting.None);
+            return JsonSerializer.Serialize(o, _serializerOptions);
+        }
+        public T Deserialize<T>(string json) {
+            return JsonSerializer.Deserialize<T>(json, _serializerOptions);
         }
 
         public event Action OnEot;
@@ -25,24 +28,28 @@ namespace Crawler3WebsocketClient {
 
         public void ProcessMessage(string message) {
             if(message is null || message.Length < 3 || message[0] != '!') {
-                _logger?.LogInfo(message ?? "<null>");
+                message ??= "<null>";
+                if (message.StartsWith("Error", StringComparison.InvariantCultureIgnoreCase)) {
+                    _logger?.LogError(message);
+                } else if(message.StartsWith("Warn", StringComparison.InvariantCultureIgnoreCase)) {
+                    _logger?.LogWarn(message);
+                } else {
+                    _logger?.LogInfo(message);
+                }
                 return;
             }
             message = message.TrimStart('!');
             try {
-                var jt = JToken.Parse(message);
-                var responseBase = jt.ToObject<CrawlerResponseBase>(_serializer);
-                //_logger.LogInfo($"Receiving message, type={responseBase.Type ?? "<null>"}");
+                var responseBase = Deserialize<CrawlerResponseBase>(message);
                 switch (responseBase.Type) {
                     case "eot": OnEot?.Invoke(); break;
-                    case "status": OnStatus?.Invoke(jt.ToObject<CrawlerResponseStatus>()); break;
-                    case "edges": OnEdges?.Invoke(jt.ToObject<CrawlerResponseEdges>()); break;
-                    case "node": OnNode?.Invoke(jt.ToObject<CrawlerResponseNode>()); break;
-                    default: break;
+                    case "status": OnStatus?.Invoke(Deserialize<CrawlerResponseStatus>(message)); break;
+                    case "edges": OnEdges?.Invoke(Deserialize<CrawlerResponseEdges>(message)); break;
+                    case "node": OnNode?.Invoke(Deserialize<CrawlerResponseNode>(message)); break;
+                    default: _logger?.LogWarn($"Unknown Message Type `{responseBase.Type ?? "<null>"}`"); break;
                 }
             }
-            catch (JsonReaderException ex) {_logger?.LogWarn($"invalid json: {message}", ex);}
-            catch (JsonSerializationException ex) {_logger?.LogWarn($"invalid json: {message}", ex);}
+            catch (JsonException ex) {_logger?.LogWarn($"invalid json: {message}", ex);}
         }
     }
 }
