de9592851cf81ad87ab4c08b6bfc6b269638471e
ff <>
2020-02-21 19:09:46 +0100

    cleanup

--- Crawler3WebsocketClient/IWebsocketLogger.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Crawler3WebsocketClient {
-    public interface IWebsocketLogger {
-        void LogInfo(string message, Exception exception = null);
-        void LogWarn(string message, Exception exception = null);
-        void LogError(string message, Exception exception = null);
-    }
-}
--- Crawler3WebsocketClient/Properties.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Crawler3WebsocketClient.Tests")]
